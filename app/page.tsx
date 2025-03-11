179959bbb622762a6608aa3c6464563c5ade01ab
Denis Tola <denis.tola22@gmail.com>
2025-03-11 20:37:44 +0100

    Fetching the data from HyGraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
 import CarsFilterOptions from "@/components/Home/CarsFilterOptions";
 import Hero from "@/components/Home/Hero";
 import SearchInputs from "@/components/Home/SearchInputs";
+import { ExportCarData } from "@/services/api";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    _getResults();
+  }, []);
+
+  const _getResults = async () => {
+    const result = await ExportCarData();
+    console.log(result);
+  };
+
   return (
     <div className="p-5 sm:p-10 md:p-20">
       <Hero />
---
+++ services/api.tsx
@@ -0,0 +1,24 @@
+import request, { gql } from "graphql-request";
+
+export const ExportCarData = async () => {
+  const query = gql`
+    query CarList {
+      carLists {
+        carAvg
+        createdAt
+        id
+        name
+        price
+        publishedAt
+        updatedAt
+        seats
+      }
+    }
+  `;
+  // Change this with the environment link from your account
+  const result = await request(
+    "https://eu-west-2.cdn.hygraph.com/content/cm7z6hquz00oi07wfpdww0lwk/master",
+    query
+  );
+  return result;
+};
