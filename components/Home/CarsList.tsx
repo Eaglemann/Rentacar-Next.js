3e8c62171043c0cf41a901621444b5ee548fdfdf
Denis Tola <denis.tola22@gmail.com>
2025-03-18 10:46:16 +0100

    Create CarBooking Modal
    1-Create the CarBookingModal Component
    2-Create the form component to be used in the carbooking modal

--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -1,15 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import React, { useState } from "react";
 import CarCard from "./CarCard";
+import BookingModal from "../CarBooking/BookingModal";
 
 function CarsList(props: any) {
+  const [selectedCar, setSelectedCar] = useState<any>([]);
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {props.carsLists?.map((car: any, idx: number) => (
-        <div key={idx}>
+        <div
+          key={idx}
+          onClick={() => {
+            const modal = document.getElementById("book_car_modal");
+            setSelectedCar(car);
+            if (modal) {
+              (modal as HTMLDialogElement).showModal();
+            }
+          }}
+        >
           <CarCard car={car} />
         </div>
       ))}
+
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <dialog id="book_car_modal" className="modal">
+        <BookingModal car = {selectedCar} />
+      </dialog>
     </div>
   );
 }
